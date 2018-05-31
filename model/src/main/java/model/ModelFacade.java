7a6d1ed27c19890be100391ed57ddb992608013f
Serhat <contact.serhatakar@gmail.com>
2018-05-31 09:15:59 +0200

    essai5

--- model/src/main/java/model/ModelFacade.java
+++ model/src/main/java/model/ModelFacade.java
@@ -9,7 +9,7 @@ import model.dao.ExampleDAO;
  * <h1>The Class ModelFacade provides a facade of the Model component.</h1>
  *
  * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
+ * @version 1.0tzer
  */
 public final class ModelFacade implements IModel {
 
